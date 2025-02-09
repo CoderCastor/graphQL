bbac2a9c3559b802f8f47869cda908fe9c64aa77
Tejas Shinde <tejasshinde@Tejass-MacBook-Pro.local>
2025-02-09 12:18:23 +0530

    prisma setup is done

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,25 +3,59 @@ import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
 import cors from "cors";
 import { json } from "body-parser";
+import { prismaClient } from "./lib/db";
 
 async function init() {
   const port = 8000;
   const app = express();
 
+  app.use(cors());
+  app.use(express.json());
+
   const gqlServer = new ApolloServer({
-    typeDefs: `
-    type User { 
-      id: ID! 
-      name: String 
+    //schema layer
+    typeDefs: `  
+    type Query {
+      hello : String
+      user(name : String) : String
     }
 
-    type Query {
-      books: [String]
+    type Mutation {
+      createUser(firstName:String!,lastName:String!,email:String!,password:String!) : Boolean
     }
   `,
     resolvers: {
       Query: {
-        books: () => [],
+        hello: () => `Hello codercastor. How are you ?`,
+        user: (_, { name }: { name: string }) => `Hey ${name}, How are you ?`,
+      },
+
+      Mutation: {
+        createUser: async (
+          _,
+          {
+            firstName,
+            lastName,
+            email,
+            password,
+          }: {
+            firstName: string;
+            lastName: string;
+            email: string;
+            password: string;
+          }
+        ) => {
+          await prismaClient.user.create({
+            data: {
+              firstName,
+              lastName,
+              email,
+              password,
+              salt: "random_salt",
+            },
+          });
+          return true;
+        },
       },
     },
   });
@@ -31,6 +65,11 @@ async function init() {
       message: `Server is running on PORT:${port}`,
     });
   });
+
+  //start the gqlServer
+  await gqlServer.start();
+
+  app.use("/graphql", expressMiddleware(gqlServer));
 
   app.listen(port, () => console.log(`Server is running on PORT:${port}`));
 }
